46dd98f0d9fa4d9d48cd8eaaf235eeca0f180bf8
StrickeX <32539745+StrickeX@users.noreply.github.com>
2018-01-24 21:49:15 +0100

    Delete JavaAutoBuild.java

--- JavaAutoBuild.java
+++
@@ -1,7 +0,0 @@
-public class JavaAutoBuild{
-
-  public static void main(String[] args) {
-    System.out.println("Hello Docker Workshop!");
-    System.out.println("Ich wurde automatisch erstellt!");
-  }
-}
