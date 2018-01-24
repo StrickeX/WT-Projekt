b7fc65beb2a36b18db2d70ec13bd36ece89939f0
StrickeX <32539745+StrickeX@users.noreply.github.com>
2018-01-24 23:28:23 +0100

    Create JavaAutoBuild.java

---
+++ JavaAutoBuild.java
@@ -0,0 +1,7 @@
+public class JavaAutoBuild {
+
+  public static void main(String[] args) {
+    System.out.println("Hallo Docker!");
+    System.out.println("Ich wurde automatisch erstellt!");
+  }
+}
